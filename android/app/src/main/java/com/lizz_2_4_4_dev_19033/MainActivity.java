c7c6b36465f3726b94869f8419deae13dc7167b6
Crowdbotics <team@crowdbotics.com>
2021-02-05 01:22:37 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/lizz_2_4_4_dev_19033/MainActivity.java
@@ -0,0 +1,15 @@
+package com.lizz_2_4_4_dev_19033;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "lizz_2_4_4_dev_19033";
+    }
+}
